b73446d242df1938be23c22ec356de448c2998ea
facundotfernandez <ftfernandez@fi.uba.ar>
2024-03-19 10:14:52 -0300

    Dev - More resources added to the list

--- data-sources/resources/news.js
+++ data-sources/resources/news.js
@@ -12,7 +12,7 @@ const data = [{
             en: "The aviation of the National Police of Peru celebrates 40 years",
             pt: "A aviação da Polícia Nacional do Peru celebra 40 anos",
             ru: "Авиация Национальной полиции Перу отмечает 40 лет"
-        }, description: {
+        }, shortDescription: {
             es: "A sus 40 años, cumplidos esta semana, la Dirección de Aviación Policial (Diravpol) de la Policía Nacional del Perú (PNP) llega convertida en una de las instituciones de su especialidad con más experiencia en guerra aérea de baja intensidad en Sudamérica.",
             en: "At its 40 years, completed this week, the Directorate of Police Aviation (Diravpol) of the National Police of Peru (PNP) emerges as one of the institutions in its field with the most experience in low-intensity aerial warfare in South America.",
             pt: "Aos seus 40 anos, completados esta semana, a Direção de Aviação Policial (Diravpol) da Polícia Nacional do Peru (PNP) emerge como uma das instituições em sua área com mais experiência em guerra aérea de baixa intensidade na América do Sul.",
@@ -36,7 +36,7 @@ const data = [{
             en: "The aviation of the National Police of Peru celebrates 40 years",
             pt: "A aviação da Polícia Nacional do Peru celebra 40 anos",
             ru: "Авиация Национальной полиции Перу отмечает 40 лет"
-        }, description: {
+        }, shortDescription: {
             es: "A sus 40 años, cumplidos esta semana, la Dirección de Aviación Policial (Diravpol) de la Policía Nacional del Perú (PNP) llega convertida en una de las instituciones de su especialidad con más experiencia en guerra aérea de baja intensidad en Sudamérica.",
             en: "At its 40 years, completed this week, the Directorate of Police Aviation (Diravpol) of the National Police of Peru (PNP) emerges as one of the institutions in its field with the most experience in low-intensity aerial warfare in South America.",
             pt: "Aos seus 40 anos, completados esta semana, a Direção de Aviação Policial (Diravpol) da Polícia Nacional do Peru (PNP) emerge como uma das instituições em sua área com mais experiência em guerra aérea de baixa intensidade na América do Sul.",
@@ -60,7 +60,31 @@ const data = [{
             en: "The aviation of the National Police of Peru celebrates 40 years",
             pt: "A aviação da Polícia Nacional do Peru celebra 40 anos",
             ru: "Авиация Национальной полиции Перу отмечает 40 лет"
-        }, description: {
+        }, shortDescription: {
+            es: "A sus 40 años, cumplidos esta semana, la Dirección de Aviación Policial (Diravpol) de la Policía Nacional del Perú (PNP) llega convertida en una de las instituciones de su especialidad con más experiencia en guerra aérea de baja intensidad en Sudamérica.",
+            en: "At its 40 years, completed this week, the Directorate of Police Aviation (Diravpol) of the National Police of Peru (PNP) emerges as one of the institutions in its field with the most experience in low-intensity aerial warfare in South America.",
+            pt: "Aos seus 40 anos, completados esta semana, a Direção de Aviação Policial (Diravpol) da Polícia Nacional do Peru (PNP) emerge como uma das instituições em sua área com mais experiência em guerra aérea de baixa intensidade na América do Sul.",
+            ru: "В свои 40 лет, достигнутые на этой неделе, Дирекция полицейской авиации (Diravpol) Национальной полиции Перу (PNP) выступает как одна из институтов в своей области с наибольшим опытом низко интенсивной воздушной войны в Южной Америке."
+        },
+    },
+    tags: ["Perú", "Zona Militar"],
+    reference: "https://www.zona-militar.com/2024/03/12/patrulleros-de-los-cielos-del-peru-la-aviacion-de-la-policia-nacional-del-peru-celebra-40-anos/",
+    image: "https://www.zona-militar.com/wp-content/uploads/2024/03/IMG-20240311-WA0036-1068x715.jpg"
+}, {
+    translations: {
+        date: {
+            es: "12 Marzo, 2024", en: "March 12, 2024", pt: "12 de Março de 2024", ru: "12 марта 2024 года"
+        }, title: {
+            es: "Patrulleros de los cielos peruanos",
+            en: "Patrollers of the Peruvian skies",
+            pt: "Patrulheiros dos céus peruanos",
+            ru: "Патрульные небес Перу"
+        }, subtitle: {
+            es: "La aviación de la Policía Nacional del Perú celebra 40 años",
+            en: "The aviation of the National Police of Peru celebrates 40 years",
+            pt: "A aviação da Polícia Nacional do Peru celebra 40 anos",
+            ru: "Авиация Национальной полиции Перу отмечает 40 лет"
+        }, shortDescription: {
             es: "A sus 40 años, cumplidos esta semana, la Dirección de Aviación Policial (Diravpol) de la Policía Nacional del Perú (PNP) llega convertida en una de las instituciones de su especialidad con más experiencia en guerra aérea de baja intensidad en Sudamérica.",
             en: "At its 40 years, completed this week, the Directorate of Police Aviation (Diravpol) of the National Police of Peru (PNP) emerges as one of the institutions in its field with the most experience in low-intensity aerial warfare in South America.",
             pt: "Aos seus 40 anos, completados esta semana, a Direção de Aviação Policial (Diravpol) da Polícia Nacional do Peru (PNP) emerge como uma das instituições em sua área com mais experiência em guerra aérea de baixa intensidade na América do Sul.",
