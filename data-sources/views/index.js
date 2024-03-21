59ff9f6b8f3a7d7836c71534cb25a0c194683245
facundotfernandez <ftfernandez@fi.uba.ar>
2024-03-21 20:32:10 -0300

    Files added - Basic structure for index and warships views

---
+++ data-sources/views/index.js
@@ -0,0 +1,109 @@
+const data = {
+    description: {
+        es: "Esta plataforma multilenguaje está dedicada a recopilar y difundir Datos, Información y Conocimiento Histórico y Técnico sobre los Buques de Guerra de Latinoamérica desde los años de la Independencia hasta el Presente.",
+        en: "This multilingual platform is dedicated to collecting and disseminating Data, Information, and Historical and Technical Knowledge about Warships of Latin America from the years of Independence to the Present.",
+        pt: "Esta plataforma multilíngue é dedicada a coletar e disseminar Dados, Informações e Conhecimento Histórico e Técnico sobre os Navios de Guerra da América Latina desde os anos da Independência até o Presente.",
+        ru: "Эта многоязычная платформа посвящена сбору и распространению данных, информации и исторических и технических знаний о военных кораблях Латинской Америки с момента независимости до настоящего времени."
+    }, id: "index", title: {
+        es: "Inicio", en: "Home", pt: "Início", ru: "Домой"
+    }, sections: [{
+        id: "navigation", title: {
+            es: "Contenidos del sitio", en: "Site contents", pt: "Conteúdo do site", ru: "Содержание сайта"
+        }, content: [{
+            id: "index", reference: "", title: {
+                es: "Inicio", en: "Home", pt: "Início", ru: "Домой"
+            }
+        }, {
+            id: "armies", reference: "armies.html", title: {
+                es: "Armadas", en: "Armies", pt: "Forças Armadas", ru: "Армии"
+            }
+        }, {
+            id: "aircraft", reference: "aircraft.html", title: {
+                es: "Aeronaves", en: "Aircraft", pt: "Aeronaves", ru: "Самолёты"
+            }
+        }, {
+            id: "warships", reference: "warships.html", title: {
+                es: "Buques", en: "Warships", pt: "Navios", ru: "Военные корабли"
+            }
+        }, {
+            id: "armaments", reference: "armaments.html", title: {
+                es: "Armamentos", en: "Armaments", pt: "Armamentos", ru: "Вооружение"
+            }
+        }, {
+            id: "technologies", reference: "technologies.html", title: {
+                es: "Tecnologías", en: "Technologies", pt: "Tecnologias", ru: "Технологии"
+            }
+        }, {
+            id: "policies", reference: "policies.html", title: {
+                es: "Políticas Navales", en: "Naval Policies", pt: "Políticas Navais", ru: "Военная политика"
+            }
+        }, {
+            id: "terms", reference: "terms.html", title: {
+                es: "Terminologías", en: "Terminologies", pt: "Terminologias", ru: "Терминологии"
+            }
+        }, {
+            id: "cinema", reference: "cinema.html", title: {
+                es: "Cinematografía y Música",
+                en: "Cinematography & Music",
+                pt: "Cinematografia e Música",
+                ru: "Кинематография и музыка"
+            }
+        }, {
+            id: "gaming", reference: "gaming.html", title: {
+                es: "Videojuegos", en: "Gaming", pt: "Videojogos", ru: "Игры"
+            }
+        }, {
+            id: "misc", reference: "misc.html", title: {
+                es: "Misceláneos", en: "Miscellaneous", pt: "Diversos", ru: "Разное"
+            }
+        }, {
+            id: "docs", reference: "docs.html", title: {
+                es: "Documentación", en: "Documentation", pt: "Documentação", ru: "Документация"
+            }
+        }, {
+            id: "links", reference: "links.html", title: {
+                es: "Enlaces Destacados", en: "Featured Links", pt: "Links em Destaque", ru: "Избранные ссылки"
+            }
+        }, {
+            id: "calculations", reference: "calculations.html", title: {
+                es: "Cálculos", en: "Calculations", pt: "Cálculos", ru: "Расчеты"
+            }
+        }, {
+            id: "about", reference: "about.html", title: {
+                es: "Proyecto Warships Wiki",
+                en: "Warships Wiki Project",
+                pt: "Projeto Warships Wiki",
+                ru: "Военные корабли Вики Проекта"
+            }
+        }, {
+            id: "faq", reference: "faq.html", title: {
+                es: "Preguntas Frecuentes",
+                en: "Frequently Asked Questions",
+                pt: "Perguntas Frequentes",
+                ru: "Часто задаваемые вопросы"
+            }
+        }, {
+            id: "our-team", reference: "our-team.html", title: {
+                es: "Nuestros integrantes", en: "Our Team", pt: "Nossa Equipe", ru: "Наша команда"
+            }
+        }, {
+            id: "collaborators", reference: "collaborators.html", title: {
+                es: "Nuestros colaboradores", en: "Our Collaborators", pt: "Nossos Colaboradores", ru: "Наши соавторы"
+            }
+        }, {
+            id: "partners", reference: "partners.html", title: {
+                es: "Vuestras colaboraciones", en: "Your Contributions", pt: "Suas Contribuições", ru: "Ваши вклады"
+            }
+        }]
+    }, {
+        id: "news", title: {
+            es: "Novedades destacadas", en: "Highlighted news", pt: "Novidades em destaque", ru: "Выделенные новости"
+        }
+    }, {
+        id: "articles", title: {
+            es: "Artículos destacados", en: "Features Articles", pt: "Artigos em destaque", ru: "Выдающиеся статьи"
+        }
+    }]
+}
+
+export {data}
---
+++ data-sources/views/warships.js
@@ -0,0 +1,197 @@
+const data = {
+    description: {
+        es: "Esta plataforma multilenguaje está dedicada a recopilar y difundir Datos, Información y Conocimiento Histórico y Técnico sobre los Buques de Guerra de Latinoamérica desde los años de la Independencia hasta el Presente.",
+        en: "This multilingual platform is dedicated to collecting and disseminating Data, Information, and Historical and Technical Knowledge about Warships of Latin America from the years of Independence to the Present.",
+        pt: "Esta plataforma multilíngue é dedicada a coletar e disseminar Dados, Informações e Conhecimento Histórico e Técnico sobre os Navios de Guerra da América Latina desde os anos da Independência até o Presente.",
+        ru: "Эта многоязычная платформа посвящена сбору и распространению данных, информации и исторических и технических знаний о военных кораблях Латинской Америки с момента независимости до настоящего времени."
+    }, id: "warships", title: {
+        es: "Buques", en: "Warships", pt: "Navios", ru: "Военные корабли"
+    }, sections: [{
+        id: "description", title: {
+            es: "Eras tecnológicas", en: "Technological eras", pt: "Eras tecnológicas", ru: "Технологические эпохи"
+        }, content: [{
+            title: {
+                es: "Era de la Vela", en: "Age of Sail", pt: "Era da Vela", ru: "Эпоха парусов"
+            }, description: {
+                es: "Hasta el siglo XIX, se trata de buques mayores a aquellos con remos y con la capacidad de transportar artillería, munición y a sus sirvientes. Esta integrada por buques del tipo Navíos, Galeones, Fragatas, Corbetas, Bergantines, Bergantines de goleta, Goletas y Pailebots. No hubo posesión de buques del tipo Navíos, Galeones, Fragatas.",
+                en: "Until the 19th century, it concerns ships larger than those with oars and with the capacity to carry artillery, ammunition, and their servants. It is composed of ships such as Ships of the line, Galleons, Frigates, Corvettes, Brigantines, Schooner brigantines, Schooners, and Pailbots. There was no possession of ships of the type Ships of the line, Galleons, Frigates.",
+                pt: "Até o século XIX, trata-se de navios maiores do que aqueles com remos e com capacidade para transportar artilharia, munições e seus servos. É composta por navios do tipo Navios de linha, Galeões, Fragatas, Corvetas, Bergantins, Bergantins escuna, Escunas e Pailebots. Não houve posse de navios do tipo Navios de linha, Galeões, Fragatas.",
+                ru: "До XIX века речь идет о судах, больших, чем те, что управлялись веслами и с возможностью перевозить артиллерию, боеприпасы и своих слуг. Она состоит из судов таких как линейные корабли, галеоны, фрегаты, корветы, бригантины, бригантины-шхуны, шхуны и парусные суда. Не было владения судами типа линейные корабли, галеоны, фрегаты."
+            }
+        }, {
+            title: {
+                es: "Era del Vapor", en: "Steam Age", pt: "Era do Vapor", ru: "Паровая эпоха"
+            }, description: {
+                es: "Durante el siglo XIX y XX, se trata de buques de mayor desplazamiento y capacidades que aquellos con remos y velas y con un sistema de propulsión a rueda o hélice. Esta integrada por buques del tipo Portaaviones, Acorazados, Monitores, Cruceros protegidos, Cruceros acorazados, Cruceros de Batalla, Cruceros ligeros, Cruceros pesados, Cruceros lanzamisiles, Vapores de ruedas, Fragatas de hélice, Cazatorpederos, Destructores, Fragatas antisubmarinas, Corbetas, Minadores, Cazaminas, Escampavías, Cañoneros costeros, Cañoneros fluviales, Pontones, Lanchas rápidas de ataque, Nodrizas de submarinos, Hospitales y Escuelas. No hubo posesión de buques del tipo Navíos, Galeones, Fragatas.",
+                en: "During the 19th and 20th centuries, it concerns ships of greater displacement and capabilities than those with oars and sails and with a wheel or propeller propulsion system. It is composed of ships such as Aircraft carriers, Battleships, Monitors, Protected cruisers, Armored cruisers, Battlecruisers, Light cruisers, Heavy cruisers, Missile cruisers, Paddle steamers, Screw frigates, Torpedo boats, Destroyers, Anti-submarine frigates, Corvettes, Minelayers, Minesweepers, Patrol boats, Coastal gunboats, River gunboats, Pontoons, Fast attack craft, Submarine tenders, Hospitals, and Schools. There was no possession of ships of the type Ships of the line, Galleons, Frigates.",
+                pt: "Durante os séculos XIX e XX, trata-se de navios de maior deslocamento e capacidades do que aqueles com remos e velas e com um sistema de propulsão a roda ou hélice. É composta por navios do tipo Porta-aviões, Couraçados, Monitores, Cruzadores protegidos, Cruzadores blindados, Cruzadores de batalha, Cruzadores leves, Cruzadores pesados, Cruzadores de mísseis, Vapores de roda, Fragatas de hélice, Torpedeiros, Contratorpedeiros, Fragatas antissubmarinas, Corvetas, Dragaminas, Varredores de minas, Patrulhas, Canhoneiras costeiras, Canhoneiras fluviais, Pontões, Embarcações de ataque rápido, Nodrizas de submarinos, Hospitais e Escolas. Não houve posse de navios do tipo Navios de linha, Galeões, Fragatas.",
+                ru: "В течение XIX и XX веков речь идет о судах с большим водоизмещением и возможностями, чем у тех, что имели весла и паруса, а также с системой привода колесом или винтом. Она состоит из судов таких как авианосцы, линейные корабли, мониторы, защищенные крейсера, бронированные крейсера, боевые крейсера, легкие крейсера, тяжелые крейсера, ракетные крейсера, паровые суда с колесами, винтовые фрегаты, торпедные катера, эсминцы, противолодочные фрегаты, корветы, минные тральщики, минные миноносцы, патрульные лодки, прибрежные бронекатера, речные бронекатера, понтонные платформы, быстрые атакующие катера, подводные буксиры, больницы и школы. Не было владения судами типа линейные корабли, галеоны, фрегаты."
+            }
+        }, {
+            title: {
+                es: "Era Actual", en: "Current Era", pt: "Era Atual", ru: "Современная эпоха"
+            }, description: {
+                es: "Desde fines del siglo XX y comienzos del presente. Está integrada por buques del tipo Portaaviones, Portahelicópteros, Portadrones, Cruceros lanzamisiles, Destructores lanzamisiles, Fragatas lanzamisiles, Submarinos Nucleares, Submarinos tradicionales, Asalto Anfibio, Lanchas rápidas de ataque, Nodrizas de submarinos y Contramedidas, Hospitales y Escuelas.",
+                en: "From the late 20th century to the present. It is composed of ships such as Aircraft carriers, Helicopter carriers, Drone carriers, Missile cruisers, Missile destroyers, Missile frigates, Nuclear submarines, Conventional submarines, Amphibious assault ships, Fast attack craft, Submarine tenders and Countermeasures, Hospitals, and Schools.",
+                pt: "Desde o final do século XX até o presente. É composta por navios como Porta-aviões, Porta-helicópteros, Porta-drones, Cruzadores de mísseis, Destructores de mísseis, Fragatas de mísseis, Submarinos nucleares, Submarinos convencionais, Navios de assalto anfíbio, Embarcações de ataque rápido, Nodrizas de submarinos e Contramedidas, Hospitais e Escolas.",
+                ru: "С конца XX века и до настоящего времени. Включает в себя корабли такого типа, как авианосцы, вертолетоносцы, дронокорабли, крейсера с ракетами, эсминцы с ракетами, фрегаты с ракетами, ядерные подводные лодки, традиционные подводные лодки, амфибийные штурмовые корабли, быстрые атакующие катера, подводные буксиры и контрмеры, больницы и школы."
+            }
+        }]
+    }, {
+        id: "nations", title: {
+            es: "Buques de Guerra por Nación",
+            en: "Warships by Nation",
+            pt: "Navios de Guerra por Nação",
+            ru: "Военные корабли по нациям"
+        }, content: [{
+            title: {
+                es: "Estados Libres o Soberanos",
+                en: "Free or Sovereign States",
+                pt: "Estados Livres ou Soberanos",
+                ru: "Свободные или суверенные государства"
+            }, subtitle: {
+                es: "Enlistado de todos los buques de una nación desde los días de la Independencia hasta el Presente",
+                en: "List of all ships of a nation from the days of Independence to the Present",
+                pt: "Lista de todos os navios de uma nação desde os dias da Independência até o Presente",
+                ru: "Список всех кораблей нации от дней независимости до настоящего времени"
+            }
+        }, {
+            title: {
+                es: "Dependencias y/o Territorios No Soberanos",
+                en: "Dependent Territories or Non-Sovereign Territories",
+                pt: "Territórios Dependentes ou Territórios Não Soberanos",
+                ru: "Зависимые территории или неполноправные территории"
+            }, subtitle: {
+                es: "Enlistado de todos los buques de una Dependencia o Territorio operados con asiento en estos lugares desde la Colonización hasta el Presente",
+                en: "List of all ships of a Dependency or Territory operated based in these locations from Colonization to the Present",
+                pt: "Lista de todos os navios de uma Dependência ou Território operados com base nesses locais desde a Colonização até o Presente",
+                ru: "Список всех судов зависимости или территории, эксплуатируемых на этих местах с колонизации до настоящего времени"
+            }
+        }]
+    }, {
+        id: "types", reference: "/warships/type.html", title: {
+            es: "Buques de Guerra por Tipo",
+            en: "Warships by Type",
+            pt: "Navios de Guerra por Tipo",
+            ru: "Военные корабли по типам"
+        }, subtitle: {
+            es: "Enlistado de buques de guerra de un mismo tipo.",
+            en: "List of warships of the same type.",
+            pt: "Lista de navios de guerra do mesmo tipo.",
+            ru: "Список военных кораблей одного типа."
+        }, content: [{
+            id: "battleship", title: {
+                es: "Acorazado", en: "Battleship", pt: "Couraçado", ru: "Линкор"
+            }
+        }, {
+            id: "amphibious", title: {
+                es: "Anfibio", en: "Amphibious", pt: "Anfíbio", ru: "Амфибийный"
+            }
+        }, {
+            id: "antique", title: {
+                es: "Antiguo", en: "Antique", pt: "Antigo", ru: "Старый"
+            }
+        }, {
+            id: "auxiliary", title: {
+                es: "Auxiliar", en: "Auxiliary", pt: "Auxiliar", ru: "Вспомогательный"
+            }
+        }, {
+            id: "gunboat", title: {
+                es: "Cañonero", en: "Gunboat", pt: "Canhoneira", ru: "Канонерская лодка"
+            }
+        }, {
+            id: "cruiser", title: {
+                es: "Crucero", en: "Cruiser", pt: "Cruzador", ru: "Крейсер"
+            }
+        }, {
+            id: "destroyer", title: {
+                es: "Destructor", en: "Destroyer", pt: "Destróier", ru: "Эсминец"
+            }
+        }, {
+            id: "escort", title: {
+                es: "Escolta", en: "Escort", pt: "Escolta", ru: "Эскорт"
+            }
+        }, {
+            id: "trainingShip", title: {
+                es: "Escuela", en: "Training Ship", pt: "Navio Escola", ru: "Учебный корабль"
+            }
+        }, {
+            id: "hybrid", title: {
+                es: "Híbrido", en: "Hybrid", pt: "Híbrido", ru: "Гибридный"
+            }
+        }, {
+            id: "minor", title: {
+                es: "Menor", en: "Minor", pt: "Menor", ru: "Малый"
+            }
+        }, {
+            id: "mineLayer", title: {
+                es: "Minador", en: "Mine layer", pt: "Minador", ru: "Минный заградитель"
+            }
+        }, {
+            id: "aircraftCarrier", title: {
+                es: "Portaaeronave", en: "Aircraft carrier", pt: "Porta-aviões", ru: "Авианосец"
+            }
+        }, {
+            id: "submersible", title: {
+                es: "Sumergible", en: "Submersible", pt: "Submersível", ru: "Подводный"
+            }
+        }]
+    }, {
+        id: "navigation", title: {
+            es: "Contenidos del sitio", en: "Site contents", pt: "Conteúdo do site", ru: "Содержание сайта"
+        }, references: [{
+            title: {
+                es: "Armadas de Latinoamérica",
+                en: "Latin American Navies",
+                pt: "Marinhas da América Latina",
+                ru: "Морские силы Латинской Америки"
+            }, reference: "armaments.html"
+        }, {
+            title: {
+                es: "Evolución de las Armadas de Latinoamérica",
+                en: "Evolution of Latin American Navies",
+                pt: "Evolução das Marinhas da América Latina",
+                ru: "Эволюция морских сил Латинской Америки"
+            }, reference: "evolution.html"
+        }, {
+            title: {
+                es: "Desplazamiento de las Armadas de Latinoamérica",
+                en: "Deployment of Latin American Navies",
+                pt: "Deslocamento das Marinhas da América Latina",
+                ru: "Развертывание морских сил Латинской Америки"
+            }, reference: "evolution.html"
+        }, {
+            title: {
+                es: "Menú principal", en: "Main Menu", pt: "Menu Principal", ru: "Главное меню"
+            }, reference: "/"
+        }]
+    }, {
+        id: "notes", title: {
+            es: "Notas de Observación", en: "Observation Notes", pt: "Notas de Observação", ru: "Заметки наблюдения"
+        }
+    }, {
+        id: "articles", title: {
+            es: "Artículos destacados", en: "Features Articles", pt: "Artigos em destaque", ru: "Выдающиеся статьи"
+        }
+    }, {
+        id: "sources", title: {
+            es: "Fuentes de Información",
+            en: "Information Sources",
+            pt: "Fontes de Informação",
+            ru: "Источники информации"
+        }
+    }, {
+        id: "authors", title: {
+            es: "Autores y Colaboradores del Artículo",
+            en: "Authors and Contributors of the Article",
+            pt: "Autores e Colaboradores do Artigo",
+            ru: "Авторы и участники статьи"
+        }
+    }, {
+        id: "editions", title: {
+            es: "Ediciones del Artículo", en: "Article Editions", pt: "Edições do Artigo", ru: "Редакции статьи"
+        }
+    }]
+}
+
+export {data}
