9c559b7be4fb1be439f4c4cde0664de8950aeda1
facundotfernandez <ftfernandez@fi.uba.ar>
2024-03-28 20:57:32 -0300

    Hotfix - Minor fix

--- scripts/views/index.js
+++ scripts/views/index.js
@@ -20,15 +20,11 @@ function createBasicStructure(container, locale) {
     const viewNavCards = createNavCards(container, sectionsData[0].title[locale], "navigation");
     createViewNavCards(viewNavCards, locale);
 
-    /*const newsNavCards = createNavCards(container, sectionsData[1].title[locale], "latest-news");
-    newsNavCards.classList.add("news-container");
-
-    const articlesNavCards = createNavCards(container, sectionsData[2].title[locale], "featured-articles");
-    articlesNavCards.classList.add("articles-container");
-    createResourcesCards(newsNavCards, articlesNavCards, locale);*/
     for (let section of viewData.sections) {
-        let article = createArticle(true, false, section.title[locale], "", section.id);
-        container.appendChild(article);
+        if (section.id !== "navigation") {
+            let article = createArticle(true, false, section.title[locale], "", section.id);
+            container.appendChild(article);
+        }
     }
     createResourcesCards(getSectionData(viewData, "featured-articles").content, "featured-articles", "article", locale);
     createResourcesCards(getSectionData(viewData, "latest-news").content, "latest-news", "news", locale);
